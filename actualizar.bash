6e1fe5e7e67e4731291fd408eabe9e5988679a7b
devikross <devikross@gmail.com>
2026-01-29 23:06:08 -0500

    update

--- actualizar.bash
+++ actualizar.bash
@@ -1,3 +1,14 @@
 #!/data/data/com.termux/files/usr/bin/bash
+
+# Ir a la carpeta de shortcuts
 cd "$HOME/.shortcuts" || exit 1
-git pull origin main
+
+echo "[INFO] Actualizando desde GitHub..."
+
+# Traer cambios remotos
+git fetch origin
+
+# Sobrescribir todo local con lo remoto
+git reset --hard origin/main
+
+echo "[INFO] Actualización completada."
