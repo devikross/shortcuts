73af512d7aad1819261bd9b5d8d399e5fd32a8f2
devikross <devikross@gmail.com>
2026-01-29 22:42:16 -0500

    update

--- paste_send.bash
+++ paste_send.bash
@@ -8,7 +8,7 @@ done
 [ -z "$MCB" ] && { echo "[ERROR] Clipboard vacío"; exit 1; }
 
 echo "[INFO] IP → leyendo ip.txt"
-IP="$(cat ../../ip.txt 2>/dev/null)"
+IP="$(cat ../ip.txt 2>/dev/null)"
 [ -z "$IP" ] && { echo "[ERROR] ip.txt no encontrado o vacío"; exit 1; }
 
 HOST="${IP%:*}"
